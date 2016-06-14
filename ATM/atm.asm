d18b617efb1bac3b74864a7f3d4c93d7dc742eb7
Christos Pylianidis <chrispyl@users.noreply.github.com>
2016-06-14 03:31:46 +0300

    Translated comments to english

--- ATM/atm.asm
+++ ATM/atm.asm
@@ -1,92 +1,92 @@
 MVI A, 0000H
-OUT 20H 			 ;αρχικοποίηση της λυχνίας λάθος κωδικού στο off
-MVI A, 0000H		 ;αρχικοποίηση της λυχνίας αναντιστοιχίας χαρτονομισμάτων που ζητάει και ποσού ανάληψης
+OUT 20H 			 ;initialize wrong PIN lamp to off
+MVI A, 0000H		         ;initialize the lamp, which shows if withrawal money and asked types don't much, to off
 OUT 21H
 
-					 ;εδώ χρειάζονται 2 είσοδοι γιατί οι κωδικοί είναι τετραψήφιοι
-IN 00H				 ;πρώτο byte κωδικού κάρτας
+				 ;here 2 inputs are needed because the codes have 4 digits
+IN 00H				 ;first byte of card code
 STA 0100H	
-IN 01H				 ;δεύτερο byte κωδικού κάρτας
+IN 01H				 ;second byte of card code
 STA 0101H
-IN 02H				 ;πρώτο byte κωδικού χρήστη
+IN 02H				 ;first byte of use PIN
 STA 0102H
-IN 03H				 ;δεύτερο byte κωδικού χρήστη
+IN 03H				 ;second byte of use PIN
 STA 0103H
 
-LHLD 0100H			 ;βάζω τον κωδικό της κάρτας στον DE και του χρήστη στον HL και τους συγκρίνω
+LHLD 0100H			 ;put the card code to DE and the user PIN to HL and compare them
 XCHG 
 LHLD 0102H
 
 MOV A,H
 CMP D
-JNZ WRONG_CODE 		 ;αν δεν συμπίπτουν ανάβει η λυχνία 
+JNZ WRONG_CODE 		 	 ;if they don't match turn on the lamp 
 MOV A,E
 CMP L
 JNZ WRONG_CODE
 
-IN 04H				 ;εισάγει ο χρήστης το ποσό της ανάληψης ανά byte 
+IN 04H				 ;the user enters the withdrawal amount per byte 
 STA 0104H
 IN 05H;
 STA 0105H
 
-IN 06 				 ;επιλέγει αν θέλει να διαλέξει αυτός τί χαρτονομίσματα θέλει (αν ναι θεωρώ είσοδο 1)
+IN 06 				 ;the user chooses if he wants to pick the paer money types (if yes, input is considered 1)
 MVI B, 0001H
 CMP B
-JZ CHOOSE_CASH 		 ;αν είναι 1, jump στην επιλογή
+JZ CHOOSE_CASH 		 	 ;if 1 jump to selection
 
-					 ;αν έφτασε εδώ επέλεξε να του δώσει ότι θέλει το μηχάνημα
-					 ;για να βρω πόσα χαρτονομίσματα θα πάρει από κάθε είδος ξεκινάω διαιρέσεις με το αντίστοιχο χαρτονόμισμα μέχρι να γίνει το υπόλοιπο αρνητικό
-MVI A, 0064H 		 ;βάζω το 100 στον Α και μετά στη μνήμη για να το αφαιρώ σε κάθε επανάληψη από το ποσό ανάληψης
+				 ;if it reached here, the user chose to not pick types of paper money
+				 ;in order to find how many paper money the user will take from each type i start divisions until the remnant becomes negative
+MVI A, 0064H 		 	 ;put 100 to A and then to memory in order to subtract it in every iteration from the withdrawal amount
 STA 0106H 
-LHLD 0104H			 ;βάζω στον HL το ποσό της ανάληψης
-SHLD 0202H			 ;το αποτέλεσμα της αφαίρεσης θα αποθηκεύεται στις θέσεις μνήμης 0202H-0203H
+LHLD 0104H			 ;load HL with the withdrawal amount
+SHLD 0202H			 ;the result of the subtraction will be saved to positions 0202H-0203H in memory
 MVI A, 0000H
-STA 0300H 			 ;αρχικοποιώ τη θέση 0300H που θα κρατάει πόσα 100ευρα θα πάρει
+STA 0300H 			 ;initialize position 0300H which will hold how many 100e the user will take
 
 
 EKATO: nop
-LHLD 0202H           ;παίρνω το ποσό που θέλω να σπάσω σε 100ευρα
-XCHG                 ;το βάζω στον DE
-LHLD 0106H           ;βάζω στον HL το 100
-MOV A, E       		 ;βάζω στον Α τα 8 λιγότερο σημαντικά bit του DE
-SUB L            	 ;αφαιρώ τα αντίστοιχα του HL από αυτά του Α
-MOV L, A             ;αποθηκεύω το αποτέλεσμα στον L
-MOV A, D             ;κάνω την ίδια διαδικασία για τα 8 σημαντικότερα bit συνυπολογίζοντας όμως και το κρατούμενο απ' την προηγούμενη αφαίρεση
+LHLD 0202H           		 ;take the amount which i want to break into 100e
+XCHG                 		 ;put it into DE
+LHLD 0106H           		 ;put 100 to HL
+MOV A, E       		 	 ;put to A the 8 LSBs of DE
+SUB L            	 	 ;subtract the corresponding of HL from the ones of Α
+MOV L, A             		 ;save the result to L
+MOV A, D             		 ;do the same procedure for the 8 MSBs, but also taking into account the carry from the previous subtraction
 SBB H                       
-MOV H, A             ;μεταφέρω το αποτέλεσμα στον H και μετά ανανεώνω τις τιμές των θέσεων 0202H-0203η
+MOV H, A             		 ;move the result to H and then refresh the values of the positions 0202H-0203H in memory
 SHLD 0202H              
 
-LDA 0300H			 ;φορτώνω και αυξάνω το πηλίκο και στη συνέχεια του ανανεώνω την τιμή στη μνήμη
+LDA 0300H			 ;load and increment the quotient and then refresh the value of the memory
 INR A
 STA 0300H 
 
-MOV A,H				 ;συγκρίνω το πιο σημαντικό byte του αριθμόυ που έμεινε απ' την αφαίρεση με το 0
+MOV A,H				 ;compare the most significant byte of the number resulted from the subtraction with 0
 MVI H, 0000H
-JZ CHECK_SECOND 	 ;αν είναι μηδέν πάω και συγκρίνω το 2 byte
-JC NEXT1			 ;αν έχει σηκωθεί carry flag σημαίνει ότι έγινε underflow άρα μπορεί να προχωρήσει παρακάτω στον υπολογισμό των 50ευρων
-JP EKATO			 ;αν είναι θετικός ο αριθμός σημαίνει ότι μπορεί να συνεχιστεί η διαίρεση
-JMP NEXT1			 ;αν δεν ισχύουν τα παραπάνω προχωράει κανονικά στον υπολογισμό των 50ευρων 
+JZ CHECK_SECOND 	 	 ;if it is zero then compare the second byte
+JC NEXT1			 ;if the carry flag is up, it means that an underflow has happened and it can continue to the calculation of the 50euros
+JP EKATO			 ;if the number is postitive it means that the division can continue
+JMP NEXT1			 ;if none of the above is true, it continues to the calculation of 50euros 
 
 CHECK_SECOND: nop
-MOV A, L			 ;ελέγχονται τα λιγότερο σημαντικά byte
+MOV A, L			 ;the least significant bytes are checked
 MVI L, 0000H
-CMP L				 ;οι έλεγχοι είναι ίδιοι με των σημαντικότερων byte
+CMP L				 ;the checks are the same tith the ones of the most significant bytes
 JC NEXT1
 JP EKATO
 JMP NEXT1
  
 
 NEXT1: nop
-LDA 0300H			 ;φορτώνω τη θέση μνήμης που είναι το πηλίκο και αφαιρώ ένα καθώς μετρήθηκε και η φορά που πήγε αρνητικό το αποτέλεσμα	
+LDA 0300H			 ;load the memory position where the quotient lies and subtract 1 because the iteration where the result got negative was taken into account	
 DCR A	
 STA 0300H
-LDA 0202H			 ;βάζω στον Α το αρνητικό αποτέλεσμα και πορσθέτω 100 για να πάρω το υπόλοιπο της διαίρεσης, χρειάζεται μόνο ο Α καθώς το υπόλοιπο είναι σίγουρα <2^8
+LDA 0202H			 ;load A with the negative result and add 100 in order to take the remnant of the division, only A is needed as the remnant is surely <2^8
 ADI 0064H	
 STA 0202H
 
-MOV A, E			 ;οι διαιρέσεις συνεχίζονται και εδώ με την ίδια λογική μόνο που αφορούν 8 bit, ο Α θα έχει από δω και πέρα το υπόλοιπο
-MVI B, 0032H		 ;ο B θα κρατάει τον αριθμό που αφαιρείται κάθε φορά (50,20,10)
-MVI E, 0000H		 ;ο E θα κρατάει από δω και πέρα τα πηλίκα
+MOV A, E			 ;the divisions continue here wiith the same logic but for 8 bit, A will hold from now on the remnant
+MVI B, 0032H		 	 ;B will hold from now on the number subtracted every time (50,20,10)
+MVI E, 0000H		 	 ;E will hold frοm now on the quotients
 
 PENHNTA: nop
 SUB B	
@@ -96,10 +96,10 @@ JP PENHNTA
 DCR E
 
 ADI 0032H
-MOV D,A				 ;αποθηκεύω στον D τον Α για να μην χάσω την τιμή όσο θα βάζω τον E στην μνήμη
+MOV D,A				 ;save A to D in order not to lose the value while i put E into memory
 
 MOV A,E
-STA 0301H			 ;η θέση 0301H έχει πόσα 50ευρα θα πάρει
+STA 0301H			 ;position 0301H has how many 50e the user will take
 
 MOV A,D
 MVI B, 0014H
@@ -115,7 +115,7 @@ ADI 0014H
 MOV D,A
 
 MOV A,E
-STA 0302H			 ;η θέση 0302H τα 20ευρα
+STA 0302H			 ;position 0302H the 20e
 
 MOV A,D
 MVI B, 000AH
@@ -128,9 +128,9 @@ JP DEKA
 DCR E
 
 MOV A,E
-STA 0303H			 ;και η 0303H τα 10ευρα
+STA 0303H			 ;position 0303H the 10e
 
-					 ;ανοιγόκλεισε κάθε διακόπτη ανάλογα
+				 ;toggle each switch the corresponding amount of times
 LDA 0300H
 OUT 10H				 ;100
 LDA 0301H
@@ -143,24 +143,24 @@ HLT
 
 CHOOSE_CASH: nop
 
-					 ;αφού διαλέξει πόσα χαρτονομίσματα θέλει απ' το καθένα γίνεται επαλήθευση αν αντιστοιχούν στο ποσό ανάληψης, ο τρόπος είναι ότι αθροίζω αυτά που ζητάει και τα συγκρίνω με το ποσό ανάληψης
-MVI H, 0000H		 ;αρχικοποιώ τους HL, DE
+				 ;after the user decides how many paer moeny he wants from each type, a verification is done to check if they add up to the withdrawal amount, i do this by adding the ones he asks and compare them with the withdrawal amount
+MVI H, 0000H		 	 ;initialize HL, DE
 MVI L, 0000H
 MVI D, 0000H
 MVI E, 0000H
 MVI A, 0000H
-STA 0260H  			 ;αρχικοποιώ την μνήμη 0260H που θα έχει το σύνολο αυτών που ζητάει
+STA 0260H  			 ;initialize position 0260H which will have the total of those the user ask for
 STA 0261H
 
-IN 07H 				 ;παίρνει πόσα 100ευρα ζητάει
+IN 07H 				 ;takes how many 100e the user asks
 STA 0300H 
-CPI 0000H			 ;αν δεν ζητάει πήγαινε στα 50ευρα
+CPI 0000H			 ;if he doesn't askgo to 50e
 JZ JUMP100
 
-LHLD 0260H			 ;φόρτωσε το σύνολο στον HL
-MVI C, 0000H		 ;ο C θα μετράει πότε να σταματήσουν οι προσθέσεις, που θα είναι όταν το C γίνει ίσο με την τιμή των χαρτονομισμάτων που ζήτησε απ' αυτό το είδος
+LHLD 0260H			 ;load HL with the total
+MVI C, 0000H		 	 ;C counts when to stop the additions, which will happen when C becomes equal to the amount of paper money of this type asked by the user
 MVI D, 0000H
-MVI E, 0064H		 ;ο DE θα χρησιμοποιηθεί σαν ζεύγος γιατί το αποτέλεσμα μπορεί να 'ναι πάνω >2^8 και έχει το ποσό που προστίθεται ανάλογα το χαρτονόμισμα
+MVI E, 0064H		 	 ;DE will be used as a pair because the result may be greater than 2^8 and it has the amount which is added depending on the paper money type
 SUM100: nop
 DAD D 
 SHLD 0260H;
@@ -168,11 +168,11 @@ INR C
 CMP C
 JNZ SUM100
 
-					 ;η ίδια διαδικασία επαναλαμβάνεται για όλα τα είδη χαρτονομισμάτων
+				 ;the same procedure is done for all the paper money types
 
 
 JUMP100: nop
-IN 08H				 ;παίρνει πόσα 50ευρα
+IN 08H				 ;takes how many 50e
 STA 0301H
 CPI 0000H
 JZ JUMP50
@@ -190,7 +190,7 @@ JNZ SUM50
 
 
 JUMP50: nop
-IN 09H				 ;παίρνει πόσα 20ευρα
+IN 09H				 ;takes how many 20e
 STA 0302H
 CPI 0000H
 JZ JUMP20
@@ -208,7 +208,7 @@ JNZ SUM20
 
 
 JUMP20: nop
-IN 0AH				 ;παίρνει πόσα 10ευρα
+IN 0AH				 ;takes how many 10e
 STA 0303H
 CPI 0000H
 JZ JUMP10
@@ -223,32 +223,32 @@ CMP C
 JNZ SUM10
 
 JUMP10: nop
-LHLD 0104H			 ;φόρτωσε στον HL το ποσό της ανάληψης
-XCHG				 ;βάλτο στον DE για να γίνει μετά η σύγκριση
-LHLD 0260H			 ;φόρτωσε το σύνολο αυτών που ζητάει
+LHLD 0104H			 ;load HL with the withdrawal amount
+XCHG				 ;put it to DE in order to do the comparison next
+LHLD 0260H			 ;load the total of the paper money types he asks
 MOV A,H
-CMP D				 ;σύγκριση των 2 bytes, αν δεν συμπίπτουν ανάβει η λυχνία στην έξοδο 21
+CMP D				 ;comparison of the 2 bytes, if they don't match the lamp at the output 21 is turned on
 JNZ WRONG_SUM
 MOV A,L
 CMP E
 JNZ WRONG_SUM
-					 ;αν είναι ίδια τα ποσά άνοιξε τον κάθε διακόπτη ανάλογα
+				 ;if the amounts match, toggle each switch the corresponding amount of times
 LDA 0300H
-OUT 10H				 ;διακόπτης 100
+OUT 10H				 ;switch 100
 LDA 0301H
-OUT 11H 			 ;διακόπτης 50
+OUT 11H 			 ;switch 50
 LDA 0302H
-OUT 12H 			 ;διακόπτης 20
+OUT 12H 			 ;switch 20
 LDA 0303H
-OUT 13H 			 ;διακόπτης 10
+OUT 13H 			 ;switch 10
 HLT
 
 WRONG_SUM: nop
-MVI  A, 0001H 		 ;άναψε τη λυχνία λάθος ποσού
+MVI  A, 0001H 		 	 ;turn on the wrong total amount of paper money types lamp
 OUT 21H
 HLT
 
 WRONG_CODE: nop
-MVI A, 0001H 		 ;αναψε τη λυχνία λάθος κωδικού
+MVI A, 0001H 		 	 ;turn on the wrong PIN lamp
 OUT 20H
 HLT
